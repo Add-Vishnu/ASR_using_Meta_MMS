3b3744e8be73191120f03eb3b18916b8335c11ee
Addanki Tirumala Vishnu Vardhan <139844342+Add-Vishnu@users.noreply.github.com>
2023-11-01 17:31:44 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import gradio as gr
+from asr import transcribe,detect_language,transcribe_lang
+
+demo = gr.Interface(transcribe,
+                   gr.inputs.Audio(source="microphone", label="Use mic"),
+                   outputs=["text","text"])
+demo2 = gr.Interface(detect_language,
+                   gr.inputs.Audio(source="microphone", label="Use mic"),
+                   outputs=["text","text"])
+demo3 = gr.Interface(transcribe_lang,
+                   inputs=[gr.inputs.Audio(source="microphone", label="Use mic"),"text"],
+                   outputs=["text","text"])
+
+tabbed_interface = gr.TabbedInterface([demo,demo2,demo3],["Transcribe by auto detecting language","Detect language","Transcribe by providing language"])
+
+with gr.Blocks() as asr:
+    tabbed_interface.render()
+asr.queue(concurrency_count=3)
+asr.launch()
